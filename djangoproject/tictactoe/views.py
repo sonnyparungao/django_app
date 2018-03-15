ba82e52e9f0cdd3766fd905f33dc7989f4521a1b
Sonny Parungao <sonny.parungao@bpocareerhub.com>
2018-03-15 21:06:55 +0800

    Added notes on the project

--- djangoproject/tictactoe/views.py
+++ djangoproject/tictactoe/views.py
@@ -30,12 +30,6 @@ def processGameWinner(request):
 
 @login_required(login_url='/')
 def users(request):
-    """
-    NOTE: This is fine for demonstration purposes, but this should be
-    refactored before we deploy this app to production.
-    Imagine how 100,000 users logging in and out of our app would affect
-    the performance of this code!
-    """
     users = User.objects.select_related('logged_in_user')
     for user in users:
         user.status = 'Online' if hasattr(user, 'logged_in_user') else 'Offline'
